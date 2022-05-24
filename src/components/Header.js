ec6ebac4c63b520690be73c4099bbc2cbf5da0be
RJCB <37097058+RJCB@users.noreply.github.com>
2022-05-23 21:02:36 -0400

    add responsive css

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,10 +9,10 @@ const Header = () => {
             <header>
                 <div className="nav-menu">
                     <Link to="/"><img src="https://www.themoviedb.org/assets/2/v4/logos/v2/blue_short-8e7b30f73a4020692ccca9c88bafe5dcb6f8a62a4c6bc55cd9ba82bb2cd95f6c.svg" alt="" role="presentation" /></Link>
-                    <nav role="navigation">
+                    {/* <nav role="navigation">
                         <button>Movies</button>
                         <button>TV Shows</button>
-                    </nav>
+                    </nav> */}
                 </div>
                 <div className="nav-login">
                     {!user && <Link to="/login">Login</Link>}
